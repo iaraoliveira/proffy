0e5581eb042ab91428fc218c3bb12982b6f8e057
Iara Oliveira <iara99oliveira@gmail.com>
2020-08-04 20:30:34 -0300

    :paintbrush: Implementa layout da landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-import './assets/styles/global.css'
+import Landing from './pages/Landing';
+
+import './assets/styles/global.css';
 
 function App() {
   return (
-    <h1>Heloooo World</h1>
+    <Landing></Landing>
   );
 }
 
---
+++ web/src/pages/Landing/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import logoImg from '../../assets/images/logo.svg';
+import landingImg from '../../assets/images/landing.svg';
+
+import studyIcon from '../../assets/images/icons/study.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
+import './styles.css';
+
+function Landing() {
+    return (
+        <div id="page-landing">
+            <div id="page-landing-content" className="container">
+                <div className="logo-container">
+                    <img src={logoImg} alt="Proffy" />
+                    <h2>Sua plataforma de estudos online</h2>
+                </div>
+
+                <img 
+                    src={landingImg} 
+                    alt="Plataforma de estudos" 
+                    className="hero-image" 
+                />
+
+                <div className="buttons-container">
+                    <a href="/" className="study">
+                        <img src={studyIcon} alt=""/>
+                        Estudar
+                    </a>
+                    <a href="/" className="give-classes">
+                        <img src={giveClassesIcon} alt=""/>
+                        Dar aulas
+                    </a>
+                </div>
+
+                <span className="total-connections">
+                    Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
+                </span>
+            </div>
+        </div>
+    );
+}
+
+export default Landing;
