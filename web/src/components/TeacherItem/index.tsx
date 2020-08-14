cd7c11069c0a762501019ee0ea4b0ea8740e31e7
iara.oliveira <iara99oliveira@gmail.com>
2020-08-13 23:17:24 -0300

    feature: Add new connection

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css';
+import api from '../../services/api';
 
 export interface Teacher {
     avatar: string;
@@ -18,6 +19,13 @@ interface TeacherItemProps {
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ( { teacher } ) => {
+
+    function createNewConnection() {
+        api.post("/connections", {
+            user_id: teacher.id
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
@@ -37,7 +45,11 @@ const TeacherItem: React.FC<TeacherItemProps> = ( { teacher } ) => {
                     Preço/hora
                     <strong>{teacher.cost}</strong>
                 </p>
-                <a href={`https://wa.me/${teacher.whatsapp}`}>
+                <a 
+                    target="_blank"
+                    onClick={createNewConnection} 
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                >
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
                 </a>
